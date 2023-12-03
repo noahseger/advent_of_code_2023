b88689f90b3976c0351d5266b9a1b9315004de9e
Noah Seger <noah.seger@gmail.com>
2023-12-03 14:57:51 -0600

    [02] Part two

--- src/02_cube_conundrum.zig
+++ src/02_cube_conundrum.zig
@@ -25,9 +25,35 @@
 // In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 //
 // Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+//
+// --- Part Two ---
+//
+// The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+//
+// As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+//
+// Again consider the example games from earlier:
+//
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+//
+//     In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+//     Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+//     Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+//     Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+//     Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+//
+// The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+// For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
 const std = @import("std");
 
 const Options = struct { bag: Bag = .{} };
+
+const Solution = struct { sum_of_possible_game_ids: u32, sum_of_power_of_minimum_set_of_cubes: u32 };
 
 const Bag = struct { red: u32 = 12, green: u32 = 13, blue: u32 = 14 };
 
@@ -37,13 +63,17 @@ pub fn main() !void {
     std.debug.print("{}\n", .{solution});
 }
 
-pub fn solve(input: []const u8, options: Options) !u32 {
-    var solution: u32 = 0;
+pub fn solve(input: []const u8, options: Options) !Solution {
+    var sum_of_possible_game_ids: u32 = 0;
+    var sum_of_power_of_minimum_set_of_cubes: u32 = 0;
     var lines = std.mem.split(u8, input, "\n");
     while (lines.next()) |line| {
         if (line.len == 0) continue;
 
         var is_possible = true;
+        var smallest_possible_red: u32 = 0;
+        var smallest_possible_green: u32 = 0;
+        var smallest_possible_blue: u32 = 0;
 
         std.debug.print("{s}\n", .{line});
 
@@ -66,20 +96,24 @@ pub fn solve(input: []const u8, options: Options) !u32 {
                 var number = try std.fmt.parseInt(u32, chunks.next().?, 10);
                 var color = chunks.next().?;
 
-                if (std.mem.eql(u8, color, "red") and number > options.bag.red) {
-                    is_possible = false;
-                } else if (std.mem.eql(u8, color, "green") and number > options.bag.green) {
-                    is_possible = false;
-                } else if (std.mem.eql(u8, color, "blue") and number > options.bag.blue) {
-                    is_possible = false;
+                if (std.mem.eql(u8, color, "red")) {
+                    if (number > options.bag.red) is_possible = false;
+                    if (smallest_possible_red < number) smallest_possible_red = number;
+                } else if (std.mem.eql(u8, color, "green")) {
+                    if (number > options.bag.green) is_possible = false;
+                    if (smallest_possible_green < number) smallest_possible_green = number;
+                } else if (std.mem.eql(u8, color, "blue")) {
+                    if (number > options.bag.blue) is_possible = false;
+                    if (smallest_possible_blue < number) smallest_possible_blue = number;
                 }
             }
         }
 
-        if (is_possible) solution += game_id;
+        if (is_possible) sum_of_possible_game_ids += game_id;
+        sum_of_power_of_minimum_set_of_cubes += smallest_possible_red * smallest_possible_green * smallest_possible_blue;
     }
 
-    return solution;
+    return .{ .sum_of_possible_game_ids = sum_of_possible_game_ids, .sum_of_power_of_minimum_set_of_cubes = sum_of_power_of_minimum_set_of_cubes };
 }
 
 test "example test" {
@@ -91,5 +125,6 @@ test "example test" {
         \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     ;
     const solution = try solve(input, .{});
-    try std.testing.expectEqual(@as(u32, 8), solution);
+    try std.testing.expectEqual(@as(u32, 8), solution.sum_of_possible_game_ids);
+    try std.testing.expectEqual(@as(u32, 2286), solution.sum_of_power_of_minimum_set_of_cubes);
 }
