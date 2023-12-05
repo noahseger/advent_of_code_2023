0832089bfc90dc0f21f4445ed2a04bd8fe8bbcc2
Noah Seger <noah.seger@gmail.com>
2023-12-05 16:01:58 -0600

    [03] Gold star

--- src/04_scratchcards.zig
+++ src/04_scratchcards.zig
@@ -34,11 +34,42 @@
 // So, in this example, the Elf's pile of scratchcards is worth 13 points.
 //
 // Take a seat in the large pile of colorful cards. How many points are they worth in total?
+//
+// --- Part Two ---
+//
+// Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
+//
+// There's no such thing as "points". Instead, scratchcards only cause you to win more scratchcards equal to the number of winning numbers you have.
+//
+// Specifically, you win copies of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+//
+// Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied. So, if you win a copy of card 10 and it has 5 matching numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you to win any more cards. (Cards will never make you copy a card past the end of the table.)
+//
+// This time, the above example goes differently:
+//
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+//
+//     Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+//     Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+//     Your copy of card 2 also wins one copy each of cards 3 and 4.
+//     Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+//     Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+//     Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+//     Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
+//
+// Once all of the originals and copies have been processed, you end up with 1 instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of card 4, 14 instances of card 5, and 1 instance of card 6. In total, this example pile of scratchcards causes you to ultimately have 30 scratchcards!
+//
+// Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end up with?
 const std = @import("std");
 
 const Options = struct { allocator: std.mem.Allocator = std.heap.page_allocator };
 
-const Solution = struct { points: u32 };
+const Solution = struct { points: u32, scratchcards: u32 };
 
 pub fn main() !void {
     const input = @embedFile("04_input.txt");
@@ -49,6 +80,10 @@ pub fn main() !void {
 pub fn solve(input: []const u8, options: Options) !Solution {
     var points: u32 = 0;
 
+    // Cache number of matches by card id for later duplication
+    var card_match_cache = std.AutoHashMap(u32, u32).init(options.allocator);
+    defer card_match_cache.deinit();
+
     var lines = std.mem.split(u8, input, "\n");
     var line_number: u32 = 0;
     while (lines.next()) |line| : (line_number += 1) {
@@ -57,9 +92,14 @@ pub fn solve(input: []const u8, options: Options) !Solution {
 
         std.debug.print("{s}\n", .{line});
         var line_chunks = std.mem.split(u8, line, ":");
-        // elide... header = Card 1
-        _ = line_chunks.next();
 
+        // Parse card_id from line header
+        var header = line_chunks.next().?;
+        var raw_card_id = std.mem.trimLeft(u8, header[4..], " ");
+        // std.debug.print("raw_card_id={s}\n", .{raw_card_id});
+        var card_id = try std.fmt.parseInt(u32, raw_card_id, 10);
+
+        // Parse matches from line body
         var body = line_chunks.next().?;
         var card_chunks = std.mem.split(u8, body, " |");
 
@@ -76,9 +116,11 @@ pub fn solve(input: []const u8, options: Options) !Solution {
         var my_numbers_chunk = card_chunks.next().?;
         var raw_my_numbers = std.mem.window(u8, my_numbers_chunk, 3, 3);
         var card_points: u32 = 0;
+        var match_count: u32 = 0;
         while (raw_my_numbers.next()) |raw_my_number| {
             std.debug.print("my_number={s}\n", .{raw_my_number});
             const is_winning = winning_numbers_cache.get(raw_my_number) orelse false;
+            if (is_winning) match_count += 1;
             if (is_winning and card_points == 0) {
                 card_points = 1;
             } else if (is_winning) {
@@ -86,10 +128,32 @@ pub fn solve(input: []const u8, options: Options) !Solution {
             }
         }
 
+        try card_match_cache.put(card_id, match_count);
         points += card_points;
     }
 
-    return .{ .points = points };
+    var card_id: u32 = 1;
+    var scratchcards: u32 = 0;
+    while (card_id <= card_match_cache.count()) : (card_id += 1) {
+        scratchcards += duplicateScratchcards(card_id, card_match_cache);
+    }
+
+    return .{ .points = points, .scratchcards = scratchcards };
+}
+
+fn duplicateScratchcards(card_id: u32, card_match_cache: std.AutoHashMap(u32, u32)) u32 {
+    // std.debug.print("card_id={}, match_count={any}\n", .{ card_id, card_match_cache.get(card_id) });
+
+    if (!card_match_cache.contains(card_id)) return 0;
+    const match_count = card_match_cache.get(card_id).?;
+
+    var scratchcards: u32 = 1;
+    var next_card_id: u32 = card_id + 1;
+    while (next_card_id <= card_id + match_count) : (next_card_id += 1) {
+        scratchcards += duplicateScratchcards(next_card_id, card_match_cache);
+    }
+
+    return scratchcards;
 }
 
 test "example test" {
@@ -103,4 +167,5 @@ test "example test" {
     ;
     const solution = try solve(input, .{});
     try std.testing.expectEqual(@as(u32, 13), solution.points);
+    try std.testing.expectEqual(@as(u32, 30), solution.scratchcards);
 }
